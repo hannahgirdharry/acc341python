9388054954a6bccbd03da0e1b61181ce164adbf1
Hannah Girdharry <hannah.girdharry92@qmail.cuny.edu>
2026-05-18 21:12:07 -0400

    your message

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Python DB")
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+cursor = conn.cursor()
+
+cursor.execute("""
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public';
+""")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.close()
+conn.close()
